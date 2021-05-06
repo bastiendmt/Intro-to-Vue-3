99786ca9bdfd1a0dec3f639b0b51464fd89a6e42
bastienxs <bastienxs@gmail.com>
2021-05-06 20:01:15 +0200

    L5

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      sizes : ["34-36","37-40","41-44"],
+      variants: [
+        { id: 34, color: " green" },
+        { id: 35, color: " blue" },
+      ],
+    };
+  },
+});
